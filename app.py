4bd69dce4bb910f6fdfcbf2bb59248a5e62710e0
Paul Novikov <49231300+pavelmetalarm@users.noreply.github.com>
2022-04-17 11:54:21 +0300

    improve template; it shows basic info about every block device in system (include flash)

--- app.py
+++ app.py
@@ -1,12 +1,20 @@
 from flask import Flask, render_template
 import fs_info
 
+
+
 app = Flask(__name__)
 
+#hardware_info = fs_info.all_my_disks[0]
+hardware_info = fs_info.all_my_disks
 
 @app.route('/info')
 def info():
-    return render_template('info.html', total=fs_info.total, free=fs_info.free, used=fs_info.used)
+    return render_template('info.html', total=fs_info.total, free=fs_info.free, used=fs_info.used,\
+           all_my_disks = hardware_info)
+
+
+
 
 
 if __name__ == "__main__":
--- fs_info.py
+++ fs_info.py
@@ -1,5 +1,14 @@
 import shutil
 import os
+import psutil
+import linux_utils
+import subprocess
+import fcntl
+import struct
+import json
+from blkinfo import BlkDiskInfo
+
+
 
 uname = os.uname()
 total = str((shutil.disk_usage("/").total // (2**30))) + "GB"
@@ -7,11 +16,43 @@ used = str((shutil.disk_usage("/").used // (2**30))) + "GB"
 free = str((shutil.disk_usage("/").free // (2**30))) + "GB"
 
 
+path1 = "/home/paul/flash"
+total1 = str((shutil.disk_usage(path1).total // (2**30))) + "GB"
+used1 = str((shutil.disk_usage(path1).used // (2**30))) + "GB"
+free1 = str((shutil.disk_usage(path1).free // (2**30))) + "GB"
+
+
+
+a = psutil.disk_partitions()
 
 
 
 
+def print_mounted_fs():
+    c = 0
+    for el in a:
+        c = c + 1
+        print(el)
+        print(c)
+
+#print(total, used, free)
 
 
+
+#print(total1, used1, free1)
+
+q = dir(os.path)
+
+
+myblkd = BlkDiskInfo()
+all_my_disks = myblkd.get_disks()
+json_output = json.dumps(all_my_disks)
+
+all_my_disks[0].pop("children",None)
+all_my_disks
+
+
+#print(type(all_my_disks[0]))
+print(all_my_disks[0])
 
 
